39e463f2fd9a7671a29a8dfc1a5d4f2335cd0bd9
Raha-73 <rahasaeidi51@gmail.com>
2024-11-04 12:48:39 -0800

    making search functionality

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,37 +1,84 @@
 import { Link } from "react-router-dom";
 import "./style.css";
-import { Fragment } from "react";
+import { Fragment, useState, useEffect } from "react";
+import axios from "axios";
+
 export default function Header() {
+  const [search, setSearch] = useState("");
+  const [searchResultVisibility, setSearchResultVisibility] = useState(false);
+  const [searchResult, setSearchResult] = useState({});
+
+  useEffect(
+    function () {
+      axios
+        .get(`https://moviesapi.codingfront.dev/api/v1/movies?q=${search}`)
+        .then(function (response) {
+          setSearchResult(response.data);
+        });
+    },
+    [search]
+  );
+
+  function renderList() {
+    return searchResult.data.map(function (item) {
+      return (
+        <Link to={`/movie/${item.id}`}>
+          <li className="search-item">{item.title}</li>
+        </Link>
+      );
+    });
+  }
+  function inputChange(event) {
+    const value = event.target.value;
+    setSearch(value);
+    setSearchResultVisibility(value.length > 0);
+  }
+
   return (
     <Fragment>
       <div className="header container-full header-wrapper justify-between flex items-center flex-nowrap">
         <div className="container justify-between flex items-center flex-nowrap">
           <div>
             <a href="/">
-              <img src="https://hotflix.volkovdesign.com/main/img/logo.svg" />
+              <img
+                src="https://hotflix.volkovdesign.com/main/img/logo.svg"
+                alt="Logo"
+              />
             </a>
           </div>
           <div className="header-links">
-            <Link className="header-link" to="#">
+            <Link className="header-link" to="/">
               HOME
             </Link>
-            <Link className="header-link" to="#">
+            <Link className="header-link" to="/catalog">
               Catalog
             </Link>
-            <Link className="header-link" to="#">
+            <Link className="header-link" to="/pricing">
               Pricing plan
             </Link>
-            <Link className="header-link" to="#">
+            <Link className="header-link" to="/pages">
               Pages
             </Link>
-            <Link className="header-link other" to="#">
+            <Link className="header-link other" to="/more">
               ...
             </Link>
           </div>
+
           <div className="input-search">
-            <input className="search" placeholder="Search..." />
+            <input
+              type="text"
+              onChange={inputChange}
+              className="search"
+              placeholder="Search..."
+              value={search}
+            />
+            {searchResultVisibility && (
+              <div className="result-search">
+                <ul className="search-list">{renderList()}</ul>
+              </div>
+            )}
           </div>
-          <Link className="nickname" to="">
+          <Link className="nickname" to="/profile">
             Nickname
           </Link>
         </div>
--- src/pages/SingleMovie/index.js
+++ src/pages/SingleMovie/index.js
@@ -4,10 +4,11 @@ import LayoutComp from "../../components/LayoutComp";
 import { Fragment, useEffect, useState } from "react";
 import axios from "axios";
 import moment from "moment-jalaali";
+import "./style.css";
 
 export default function SingleMovie() {
   const { id } = useParams();
-  const[movie, setMovie] = useState({
+  const [movie, setMovie] = useState({
     id: 0,
     title: "",
     poster: "",
@@ -27,59 +28,59 @@ export default function SingleMovie() {
     imdb_id: "",
     type: "",
     genres: [],
-    images: []
-    })
-  const[loading, setLoading] = useState(false)
+    images: [],
+  });
 
-  useEffect(function(){
-    setLoading(true)
-    axios.get(`https://moviesapi.codingfront.dev/api/v1/movies/${id}`)
-    .then(function(response){
-      setMovie(response.data)
-      setLoading(false)
-    })
-    .catch(function(error){
-      console.log(error)
-      setLoading(false)
-    })
-  },[])
+  const [loading, setLoading] = useState(false);
 
-  useEffect(function(){
-    document.title = movie.title
-  },[movie])
+  useEffect(function () {
+    setLoading(true);
+    axios
+      .get(`https://moviesapi.codingfront.dev/api/v1/movies/${id}`)
+      .then(function (response) {
+        setMovie(response.data);
+        setLoading(false);
+      })
+      .catch(function (error) {
+        console.log(error);
+        setLoading(false);
+      });
+  }, []);
 
-  function renderGenres(){
-    return movie.genres.map(function(item){
-      return(
-        <div className="movie-genre">{item}</div>
-      )
-    })
+  useEffect(
+    function () {
+      document.title = movie.title;
+    },
+    [movie]
+  );
+
+  function renderGenres() {
+    return movie.genres.map(function (item) {
+      return <div className="movie-genre">{item}</div>;
+    });
   }
 
   return (
     <Fragment>
       <LayoutComp>
+        {loading ? (
+          <h2 className="loading">Loading ... please wait</h2>
+        ) : (
+          <Fragment>
+            <div className="container">
+              <div>movie detail page</div>
+              <div>page id is: {id}</div>
 
-        {
-          loading ? (<h2>Is Loading ...</h2>) : (
-            <Fragment>
-              <div className="container">
-                <div>movie detail page</div>
-                <div>page id is: {id}</div>
-
-                <h1>{movie.title}</h1>
-                <img src={movie.poster} />
-                <h2>{movie.actors}</h2>
-                <h3>{movie.plot}</h3>
-                <h2>Movie genre: </h2>
-                <div className="genres">
-                  {renderGenres()}
-                </div>
-                <h1>{moment(movie.released).format('jYYYY/jMM/jDD')}</h1>
-              </div>
-            </Fragment>
-          )
-        }
+              <h1>{movie.title}</h1>
+              <img src={movie.poster} />
+              <h2>{movie.actors}</h2>
+              <h3>{movie.plot}</h3>
+              <h2>Movie genre: </h2>
+              <div className="genres">{renderGenres()}</div>
+              <h1>{moment(movie.released).format("jYYYY/jMM/jDD")}</h1>
+            </div>
+          </Fragment>
+        )}
       </LayoutComp>
     </Fragment>
   );
